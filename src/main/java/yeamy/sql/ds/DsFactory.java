52f7c634e6cb504520164e6dec004bcdb5cf0c90
Yeamy0754 <yeamy0754@hotmail.com>
2022-11-21 01:30:35 +0800

    adapt jdk9+

--- src/main/java/yeamy/sql/ds/DsFactory.java
+++ src/main/java/yeamy/sql/ds/DsFactory.java
@@ -3,6 +3,7 @@ package yeamy.sql.ds;
 import yeamy.sql.DsIgnore;
 import yeamy.sql.DsObserver;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,12 +19,20 @@ public class DsFactory<T> {
 	private HashMap<Class<?>, DsAdapter> map;
 	private final List<DsField> fields;
 	private final Class<T> type;
+	private final Constructor<T> constructor;
 
 	/**
 	 * @param type type to generate
 	 */
 	public DsFactory(Class<T> type) {
 		this.type = type;
+		Constructor<T> constructor;
+		try {
+			constructor = type.getDeclaredConstructor();
+		} catch (Exception e) {
+			constructor = null;
+		}
+		this.constructor = constructor;
 		LinkedList<DsField> list = new LinkedList<>();
 		Field[] fields = type.getFields();
 		for (Field field : fields) {
@@ -86,8 +95,24 @@ public class DsFactory<T> {
 		return fields;
 	}
 
+	@SuppressWarnings("deprecation")
 	T read(ResultSet rs, List<DsField> list) throws SQLException, InstantiationException, IllegalAccessException {
-		T t = type.newInstance();
+		T t = null;
+		if (constructor != null) {
+			try {
+				t = type.getDeclaredConstructor().newInstance();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (t == null) {
+			try {
+				t = type.newInstance();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
 		for (DsField f : list) {
 			f.read(rs, t);
 		}
